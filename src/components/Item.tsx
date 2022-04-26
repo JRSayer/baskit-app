55d9989b78abab57d9332c6718031d8e9a17836f
JRSayer <jackryansayer@gmail.com>
2022-04-26 20:59:03 +0100

    Item styling + button

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
+
+interface RootState {
+    categoriesData: Array<object>
+};
 
 type ItemProps = {
     itemInfo: {
+        itemCategory: string,
         itemId: string,
         itemName: string,
         itemNotes: string,
@@ -13,22 +19,59 @@ type ItemProps = {
 };
 
 function Item(props: ItemProps) {
+    const [selectedFlag, setSelectedFlag] = useState(false);
+    const categoriesData:any = useSelector((state: RootState) => state.categoriesData)
+
+    const itemCategoryColor:any = categoriesData.filter(function (it:any) {
+        return it.categoryId == props.itemInfo.itemCategory
+    })
+
+    const categoryColor:string = itemCategoryColor[0].categoryColor
+    
     return (
         <View style={itemStyle.itemContainer}>
-            <Text>{props.itemInfo.itemName}</Text>
-            {props.itemInfo.itemNotes != '' ? (
-                <Text>{props.itemInfo.itemNotes}</Text>
-            ):(<></>)}
-            <Text>Wanted: {props.itemInfo.itemQuantityWanted}</Text>
+            {!selectedFlag ? (
+                <TouchableOpacity style={[itemStyle.itemNotSelectedButton, {borderColor: categoryColor}]}
+                    onPress={() => setSelectedFlag(true)}
+                ></TouchableOpacity>
+            ):(
+                <TouchableOpacity style={[itemStyle.itemSelectedButton, {backgroundColor: categoryColor}]}
+                    onPress={() => setSelectedFlag(false)}
+                ></TouchableOpacity>
+            )}
+            <View style={itemStyle.itemContentContainer}>
+                <Text>{props.itemInfo.itemName}</Text>
+                {props.itemInfo.itemNotes != '' ? (
+                    <Text>{props.itemInfo.itemNotes}</Text>
+                ):(<></>)}
+                <Text>Wanted: {props.itemInfo.itemQuantityWanted}</Text>
+            </View>
         </View>
     )
 };
 
 const itemStyle = StyleSheet.create({
     itemContainer: {
-        borderColor: 'black',
-        borderWidth: 1,
-        margin: 2,
+        marginBottom: 4,
+        marginTop: 4,
+        flexDirection: 'row',
+    },
+    itemContentContainer: {
+        backgroundColor: 'lime'
+    },
+    itemNotSelectedButton: {
+        borderWidth: 5,
+        height: 54,
+        width: 54,
+        borderRadius: 54/2,
+        marginRight: 8
+    },
+    itemSelectedButton: {
+        backgroundColor: '#feda23',
+        height: 54,
+        width: 54,
+        borderRadius: 54/2,
+        marginRight: 8
     }
 });
 
